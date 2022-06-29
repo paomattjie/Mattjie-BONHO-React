e95266832b8b535d87e89dd9b4f4e53364cbf768
paomattjie <paolamattjie@hotmail.com>
2022-06-28 21:08:12 -0300

    cambios recomendados

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,24 @@
+import react from 'react';
+import ItemCount from './ItemCount/ItemCount.jsx';
+
+import './Item/Item.css'
+
+const Item = ({name, thumbnail, price, id, stock}) => {
+
+    const onAdd = (qty) => {
+        alert (`Has agregado ${qty} kilos`);
+    };
+
+    return (
+        <article className= "product-card">
+            <img className= "product-card__image" src={thumbnail} alt="" />
+
+            <h2 className= "porduct-card__name"> {name} </h2>
+            <span className= "porduct-card__name"> ${price} </span>
+
+            <ItemCount stock={stock} onAdd={onAdd} initial={1}/>
+        </article>
+    );
+};
+
+export default Item;
